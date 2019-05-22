cb678d27fc91d5db7b18d86f5e49ac6b1d0bce50
mlee <Michael Jonathan Lee>
2019-05-22 18:56:11 +0200

    Added js version for inline styling.

---
+++ style.js
@@ -0,0 +1,18 @@
+const colors = {
+  base:   '#002D5C',
+  level1: '#133A69',
+  level2: '#2D4977',
+  level3: '#455A85',
+  level4: '#5B6D95',
+  level5: '#7382A5',
+  level6: '#8E98B7',
+  level7: '#A8AFC8',
+  level8: '#C3C8DA',
+  level9: '#E0E2EC',
+  iue:    '#E30015',
+}
+
+export default {
+  colors,
+  font: 'Verdana, sans-serif',
+}
